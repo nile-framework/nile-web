fb3c3cfc8159363c8fa00781dca254fb0aba2aca
Kevin Towe <kevintowe27@gmail.com>
2017-09-21 11:16:23 -0600

    moved content from supaged onto home page and put the correct content in the sub pages.

---
+++ src/app/apply-driver/apply-driver/apply-driver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-driver',
+  templateUrl: './apply-driver.component.html',
+  styleUrls: ['./apply-driver.component.css']
+})
+export class ApplyDriverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
