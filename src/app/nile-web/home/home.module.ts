23c5950f8fb40926699fec0dfd83c539f6e88362
Henry Tobolka <HenryT@Henrys-MacBook-Pro.local>
2017-09-22 12:43:22 -0500

    Landing Form

--- src/app/nile-web/home/home.module.ts
+++ src/app/nile-web/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ComponentsModule } from '../home/components/components.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ],
   declarations: [HomeComponent]
 })
 export class HomeModule { }
+ 
