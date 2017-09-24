21b399913601eb688648de5b964576297fffd7eb
kevintowe <kevintowe27@gmail.com>
2017-09-24 16:58:41 -0600

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const appRoutes: Routes = [
-	// { path: '', loadChildren: 'app/nile-web/nile-web.module#NileWebModule'},
-	{ path: '', redirectTo: '/admin-portal', pathMatch: 'full'},
+	{ path: '', loadChildren: 'app/nile-web/nile-web.module#NileWebModule'},
+	// { path: '', redirectTo: '/admin-portal', pathMatch: 'full'},
 	{ path: 'client-apply', loadChildren: 'app/apply-client/apply-client.module#ApplyClientModule' },
 	{ path: 'admin-portal', loadChildren: 'app/admin-portal/admin-portal.module#AdminPortalModule' },
 	// add more root routes here.
