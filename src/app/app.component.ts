4c547369ef741577047abf480fcd966d0f18aebd
kevintowe <kevintowe27@gmail.com>
2017-09-25 10:58:47 -0500

    When navigating to a new page, the window scrolls to the top of the page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor( 
+    private router: Router
+  ) {
+
+  }
+
+  // when navigating to a new page, we scroll to the top of the page.
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
+  }
+
+
   
 }
